7a3c695484cda4d1f5210ad3a0becbc1393fc963
Maria Viana do Nascimento <mariaviana05091997@gmail.com>
2025-04-29 22:47:25 -0300

    adiciona opção para informar volumes refrigerados (boolean)

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,10 @@ public class Main {
 
         System.out.println("Digite o nome do cliente: ");
         String nomeCliente = scanner.nextLine();
+
+        System.out.println("Tem volumes para os refrigeradores? (Sim/Não)");
+        String resposta = scanner.nextLine().trim().toLowerCase();
+
+        boolean frios = resposta.equals("sim");
     }
 }
